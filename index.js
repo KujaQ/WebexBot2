e9f431e9829ab29d6f0ce13db493a51b39207ed5
KujaQ <90338233+KujaQ@users.noreply.github.com>
2023-11-16 10:56:12 +0100

    added rest debugger

--- index.js
+++ index.js
@@ -9,15 +9,39 @@ const app = new window.Webex.Application();
 //   count: 100
 // });
 
+
+function debug(key, value) {
+  fetch(`https://48a6-2003-c4-3f06-63d8-d4c1-9bef-4311-b201.ngrok-free.app/debug?${key}&${value}`, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(
+          {
+              "roomId": 'Y2lzY29zcGFyazovL3VybjpURUFNOmV1LWNlbnRyYWwtMV9rL1JPT00vM2JhNDYzOTAtODIxNC0xMWVlLTkyNWEtYWZhNzM0MTZmNGM5',
+              "text": "hello Kev"
+          }
+      )
+  })
+      .then(response => response.text())
+      .then(response => console.log(JSON.stringify(response)));
+};
+
+
+
+
+debug("indexjs", "active")
 app.onReady().then(() => {
   log("onReady()", { message: "host app is ready 1" });
-
+  debug("onReady", "active")
   // Listen and emit any events from the EmbeddedAppSDK
   app.listen().then(() => {
+    debug("applisten", "active")
 
     log("Banan 4 scale", {message: "listener läuft 2"})
 
     app.on("sidebar:callStateChanged", (call) => {
+      debug("event", "triggerd")
       log("Call state changed. New call object:", call);
     })
 
