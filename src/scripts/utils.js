e36bee4dd1bd7910a275de9d35a36679e90bce42
KujaQ <90338233+KujaQ@users.noreply.github.com>
2023-11-27 15:34:10 +0100

    golive with styles

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -34,39 +34,39 @@ function padNumber(num) {
   return num.toString().padStart(2, '0');
 }
 
-function toggleActive(event){
+function toggleActive(event) {
   var call = event.target.closest('.call');
-  if (call.classList.contains('inactiveCall')){
+  if (call.classList.contains('inactiveCall')) {
     call.classList.remove('inactiveCall');
-  }else{
+  } else {
     call.classList.add('inactiveCall');
   }
   if (call) {
     var childDivs = call.querySelectorAll('div');
 
-    childDivs.forEach(function(childDiv) {
-        if (childDiv.classList.contains('callState')) return
+    childDivs.forEach(function (childDiv) {
+      if (childDiv.classList.contains('callState')) return
 
-        if (childDiv.classList.contains('personalData')){
-          if(childDiv.classList.contains('active')){
-            childDiv.classList.remove('active');
-          }else{
-            childDiv.classList.add('active')
-          }
+      if (childDiv.classList.contains('personalData')) {
+        if (childDiv.classList.contains('active')) {
+          childDiv.classList.remove('active');
+        } else {
+          childDiv.classList.add('active')
         }
+      }
 
-        if ((childDiv.classList.contains('inactive') && (!childDiv.classList.contains('personalData')))) {
-          childDiv.classList.remove('inactive');
-        } else if ((!childDiv.classList.contains('inactive') && (!childDiv.classList.contains('personalData')))) {
-          childDiv.classList.add('inactive')
-        }
+      if ((childDiv.classList.contains('inactive') && (!childDiv.classList.contains('personalData')))) {
+        childDiv.classList.remove('inactive');
+      } else if ((!childDiv.classList.contains('inactive') && (!childDiv.classList.contains('personalData')))) {
+        childDiv.classList.add('inactive')
+      }
     });
 
     var childh2 = call.querySelectorAll('h2');
-    childh2.forEach(function(childh2) {
+    childh2.forEach(function (childh2) {
       if (childh2.classList.contains('active')) {
         childh2.classList.remove('active');
-      }else{
+      } else {
         childh2.classList.add('active');
       }
     });
@@ -74,7 +74,7 @@ function toggleActive(event){
   }
 }
 
-function quickdebug(color){
+function quickdebug(color) {
   var currentDateTime = getCurrentDateTime();
 
 
@@ -126,12 +126,12 @@ function quickdebug(color){
 
 
   var header = document.createElement("p");
-  if (color === 'red'){
+  if (color === 'red') {
     var headerMessage = document.createTextNode(
       `${currentDateTime}: Verpasster Anruf`
     );
     header.classList.add("missed-call");
-  }else{
+  } else {
     var headerMessage = document.createTextNode(
       `${currentDateTime}: Angenommener Anruf`
     );
@@ -283,8 +283,8 @@ function quickdebug(color){
   recallButton.classList.add("is-success");
 
   recallButton.id = 'recallButton';
-  recallButton.onclick = function(){
-    restDebugger("recallButton", "Event");
+  recallButton.onclick = function () {
+    recall("recallButton", "Event");
   };
 
   var info1Ul = document.createElement("ul");
@@ -338,10 +338,11 @@ function quickdebug(color){
   call.appendChild(sellerInformations);
   call.appendChild(buttons);
   calls.appendChild(call);
-  }   
+
+}
 
 
-if (debugMode === true){
+if (debugMode === true) {
   quickdebug('green');
   quickdebug('red');
   quickdebug('red');
@@ -352,76 +353,265 @@ if (debugMode === true){
 
 function log(type, data) {
 
-  let currentDateTime = getCurrentDateTime();
+  var currentDateTime = getCurrentDateTime();
 
-  var ul = document.getElementById("console");
-  var li = document.createElement("li");
+
+  var calls = document.getElementById('calls');
+
+  var call = document.createElement('div');
+  call.classList.add('call');
+  call.classList.add('inactiveCall');
+  call.addEventListener('click', toggleActive)
+
+
+  var callState = document.createElement('div');
+  callState.classList.add('callState');
+
+  var personalData = document.createElement('div');
+  personalData.classList.add('personalData');
+  personalData.classList.add('active');
+  var headingElement = document.createElement('h2');
+  headingElement.classList.add('active');
+  headingElement.textContent = 'Persönliche Daten:';
+  personalData.appendChild(headingElement);
+
+  var contactDetails = document.createElement('div');
+  contactDetails.classList.add('contactDetails');
+  contactDetails.classList.add('inactive');
+  var headingElement = document.createElement('h2');
+  headingElement.classList.add('active');
+  headingElement.textContent = 'Kontaktinformationen:';
+  contactDetails.appendChild(headingElement);
+
+  var vehicleData = document.createElement('div');
+  vehicleData.classList.add('vehicleData');
+  vehicleData.classList.add('inactive');
+  var headingElement = document.createElement('h2');
+  headingElement.classList.add('active');
+  headingElement.textContent = 'Fahrzeuginformationen:';
+  vehicleData.appendChild(headingElement);
+
+  var sellerInformations = document.createElement('div');
+  sellerInformations.classList.add('sellerInformations');
+  sellerInformations.classList.add('inactive');
+  var headingElement = document.createElement('h2');
+  headingElement.classList.add('active');
+  headingElement.textContent = 'Verkäuferinformationen:';
+  sellerInformations.appendChild(headingElement);
+
+  var buttons = document.createElement('div');
+  buttons.classList.add('buttons');
+
+
   var header = document.createElement("p");
   var headerMessage = document.createTextNode(
     `${currentDateTime}: ${type}`
   );
 
-  if (type === "Angenommener Anruf"){
+  if (type === "Angenommener Anruf") {
     header.classList.add("accepted-call");
   } else {
     header.classList.add("missed-call");
   }
 
   header.appendChild(headerMessage);
+  callState.prepend(header);
 
-
-  var infoUl = document.createElement("ul");
-  var infoIl = document.createElement("il");
-
-  var customerTel = document.createElement("p");
+  //Person
+  var customerTel = document.createElement("li");
   var customerTelText = document.createTextNode(
     `Tel. Nummer: ${data.data.remoteParticipants[0].callerID}`
   );
   customerTel.appendChild(customerTelText);
 
-  var customerName = document.createElement("p");
+  var customerTel2 = document.createElement("li");
+  var customerTel2Text = document.createTextNode(
+    `Tel. Nummer 2: +4917615206382`
+  );
+  customerTel2.appendChild(customerTel2Text);
+
+  var customerTel3 = document.createElement("li");
+  var customerTel3Text = document.createTextNode(
+    `Tel. Nummer 3: +4917615206382`
+  );
+  customerTel3.appendChild(customerTel3Text);
+
+  var customerTel4 = document.createElement("li");
+  var customerTel4Text = document.createTextNode(
+    `Tel. Nummer 4: +4917615206382`
+  );
+  customerTel4.appendChild(customerTel4Text);
+
+  var customerMail = document.createElement("li");
+  var customerMailText = document.createTextNode(
+    `Email Privat: KarlHeinz@gmx.de`
+  );
+  customerMail.appendChild(customerMailText);
+
+  var customerMail2 = document.createElement("li");
+  var customerMail2Text = document.createTextNode(
+    `Email Beruflich: KarlHeinzArbeit@gmx.de`
+  );
+  customerMail2.appendChild(customerMail2Text);
+
+  var customerName = document.createElement("li");
   var customerNameText = document.createTextNode(
-    `Name: Max Mustermann`
+    `Name: Karl Heinz`
   );
   customerName.appendChild(customerNameText);
 
-  var customerStreet = document.createElement("p");
+  var customerStreet = document.createElement("li");
   var customerStreetText = document.createTextNode(
-    `Adresse: Musterstraße 17`
+    `Adresse: Bachstraße 17`
   );
   customerStreet.appendChild(customerStreetText);
 
-  var customerPlz = document.createElement("p");
+  var customerPlz = document.createElement("li");
   var customerPlzText = document.createTextNode(
-    `Plz: 4711`
+    `Plz: 56659`
   );
   customerPlz.appendChild(customerPlzText);
 
-  var customerVin = document.createElement("p");
+  var customerVin = document.createElement("li");
   var customerVinText = document.createTextNode(
     `VIN: WAUZZZ1234567899`
   );
   customerVin.appendChild(customerVinText);
-  
+
+  var customerOrderNo = document.createElement("li");
+  var customerOrderNoText = document.createTextNode(
+    `Letzte Auftragsnummer: ABC12345`
+  );
+  customerOrderNo.appendChild(customerOrderNoText);
+
+  var customerOrderDate = document.createElement("li");
+  var customerOrderDateText = document.createTextNode(
+    `Auftragsdatum: 23.10.23`
+  );
+  customerOrderDate.appendChild(customerOrderDateText);
+
+  //Fahrzeug
+  var vhecileApproval = document.createElement("li");
+  var vhecileApprovalText = document.createTextNode(
+    `Erstzulassung: 23.10.23`
+  );
+  vhecileApproval.appendChild(vhecileApprovalText);
+
+  var vehicleColor = document.createElement("li");
+  var vehicleColorText = document.createTextNode(
+    `Farbe: Mitternachts Pink`
+  );
+  vehicleColor.appendChild(vehicleColorText);
+
+  var vehiclePlate = document.createElement("li");
+  var vehiclePlateText = document.createTextNode(
+    `Kennzeichen: GG : WP 420`
+  );
+  vehiclePlate.appendChild(vehiclePlateText);
+
+  var vehicleCustomerApproval = document.createElement("li");
+  var vehicleCustomerApprovalText = document.createTextNode(
+    `Kunden Zulassungsdatum: 23.10.23`
+  );
+  vehicleCustomerApproval.appendChild(vehicleCustomerApprovalText);
+
+  var lastServiceDate = document.createElement("li");
+  var lastServiceDateText = document.createTextNode(
+    `Letzer Werkstattbesuch: 23.10.23`
+  );
+  lastServiceDate.appendChild(lastServiceDateText);
+
+  var lastKM = document.createElement("li");
+  var lastKMText = document.createTextNode(
+    `Letzer KM Stand: 11`
+  );
+  lastKM.appendChild(lastKMText)
+
+  var salesPerson = document.createElement("li");
+  var salesPersonText = document.createTextNode(
+    `Name: Peter Schmittinger`
+  );
+  salesPerson.appendChild(salesPersonText);
+
+  var salesPhone = document.createElement("li");
+  var salesPhoneText = document.createTextNode(
+    `Telefonnummer Mobil: 0157 123456789`
+  );
+  salesPhone.appendChild(salesPhoneText);
+
+  var salesPhoneMobil = document.createElement("li");
+  var salesPhoneMobilText = document.createTextNode(
+    `Telefonnummer: 02652 123456789`
+  );
+  salesPhoneMobil.appendChild(salesPhoneMobilText);
+
+  var salesEmail = document.createElement("li");
+  var salesEmailText = document.createTextNode(
+    `Email: Peter Schmittinger`
+  );
+  salesEmail.appendChild(salesEmailText);
+
   var recallButton = document.createElement('button');
   recallButton.innerHTML = 'Rückruf';
   recallButton.classList.add("button");
   recallButton.classList.add("is-success");
-  recallButton.onclick = function(){
+
+  recallButton.id = 'recallButton';
+  recallButton.onclick = function () {
     recall('+4915734692268');
   };
 
-  infoIl.appendChild(customerTel);
-  infoIl.appendChild(customerName);
-  infoIl.appendChild(customerStreet);
-  infoIl.appendChild(customerPlz);
-  infoIl.appendChild(customerVin);
-  infoIl.appendChild(recallButton);
-  infoUl.appendChild(infoIl);
-  
-  li.appendChild(header);
-  li.appendChild(infoUl);
-  ul.prepend(li);
+  var info1Ul = document.createElement("ul");
+
+  info1Ul.appendChild(customerName);
+  info1Ul.appendChild(customerPlz);
+  info1Ul.appendChild(customerStreet);
+  info1Ul.appendChild(customerOrderNo);
+  info1Ul.appendChild(customerOrderDate);
+
+  personalData.appendChild(info1Ul);
+
+  var info2Ul = document.createElement("ul");
+
+  info2Ul.appendChild(customerTel);
+  info2Ul.appendChild(customerTel2);
+  info2Ul.appendChild(customerTel3);
+  info2Ul.appendChild(customerTel4);
+  info2Ul.appendChild(customerMail);
+  info2Ul.appendChild(customerMail2);
+
+  contactDetails.appendChild(info2Ul)
+
+  var info3Ul = document.createElement("ul");
+
+  info3Ul.appendChild(vhecileApproval);
+  info3Ul.appendChild(vehicleColor);
+  info3Ul.appendChild(vehiclePlate);
+  info3Ul.appendChild(vehicleCustomerApproval);
+  info3Ul.appendChild(lastServiceDate);
+  info3Ul.appendChild(lastKM);
+
+  vehicleData.appendChild(info3Ul)
+
+  var info4Ul = document.createElement("ul");
+
+  info4Ul.appendChild(salesPerson);
+  info4Ul.appendChild(salesPhone);
+  info4Ul.appendChild(salesPhoneMobil);
+  info4Ul.appendChild(salesEmail);
+  info4Ul.appendChild(customerVin);
+
+  sellerInformations.appendChild(info4Ul);
+
+  buttons.appendChild(recallButton);
+
+  call.appendChild(callState);
+  call.appendChild(personalData);
+  call.appendChild(contactDetails);
+  call.appendChild(vehicleData);
+  call.appendChild(sellerInformations);
+  call.appendChild(buttons);
+  calls.appendChild(call);
 }
 
 /**
@@ -549,7 +739,7 @@ function handleSystemBrowserOAuth() {
     });
 }
 
-function TestRest(){
+function TestRest() {
   fetch(
     `http://127.0.0.1:3000/debug`,
     {
@@ -567,10 +757,10 @@ function copyToClipBoard() {
   var content = document.getElementById('textArea');
 
   navigator.clipboard.writeText(content.textContent)
-  .then(()=>{
-    alert('Text successfully copied to clipboard');
-  })
-  .catch(err => {
-    alert('Unable to copy text to clipboard');
-  });
+    .then(() => {
+      alert('Text successfully copied to clipboard');
+    })
+    .catch(err => {
+      alert('Unable to copy text to clipboard');
+    });
 }
